e3ca97d22e33e087557082b82d4d3df948e48035
a.gorkovenko <a.gorkovenko@tbank.ru>
2024-09-24 17:08:50 +0700

    seminar 4

---
+++ seminar4/src/main/java/backend/academy/seminar4/nulls/service/UserService.java
@@ -0,0 +1,29 @@
+package backend.academy.seminar4.nulls.service;
+
+import backend.academy.seminar4.nulls.model.User;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+public class UserService {
+
+    private final Map<Integer, User> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.put(user.getId(), user);
+    }
+
+    public User findUserById(int userId) {
+        return users.get(userId);
+    }
+
+    public String getUserEmail(int userId) {
+        return findUserById(userId).getEmail();
+    }
+
+    public String getUserName(int userId) {
+        return findUserById(userId).getName();
+    }
+}
